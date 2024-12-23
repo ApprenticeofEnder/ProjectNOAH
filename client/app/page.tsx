ec6bb22d6d5c9abdd27f7d7dda7230e7b2db17a5
Robert Babaev <github@robertbabaev.tech>
2024-12-23 03:16:29 -0500

    initialize NextJS client

---
+++ client/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Under Construction</h1>
+      <p>{`Project NOAH's front end is under construction! Check back soon.`}</p>
+    </div>
+  );
+}
